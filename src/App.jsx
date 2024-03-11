ed11e0c77eb0b03bcd03bbc24e4f36ee9c2efb12
saekane <162076053+saekane@users.noreply.github.com>
2024-03-11 19:07:20 +0900

    Update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Reacts
+            Learn React
           </a>
         </p>
       </header>
